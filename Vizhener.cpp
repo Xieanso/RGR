0bbee554791f428d85ae05ae8f13d14d273ff5ec
Xieanso <104617049+Xieanso@users.noreply.github.com>
2025-05-22 08:04:56 +0300

    Update Vizhener.cpp

--- Vizhener.cpp
+++ Vizhener.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// функция получения кода символа 
+// ГґГіГ­ГЄГ¶ГЁГї ГЇГ®Г«ГіГ·ГҐГ­ГЁГї ГЄГ®Г¤Г  Г±ГЁГ¬ГўГ®Г«Г  
 int keycode(char s) {
     s = toupper(s);
     for (int i = 0; i < alphabet.length(); i++) {
         if (s == alphabet[i]) return i;
     }
-    return -1;   // возвращает - 1 для символов не из алфавита
+    return -1;   // ГўГ®Г§ГўГ°Г Г№Г ГҐГІ - 1 Г¤Г«Гї Г±ГЁГ¬ГўГ®Г«Г®Гў Г­ГҐ ГЁГ§ Г Г«ГґГ ГўГЁГІГ 
 }
 
 string VizhenerEncode(string text, string key) {
@@ -21,7 +21,7 @@ string VizhenerEncode(string text, string key) {
         char c = text[i];
         char k = key[i % key.length()];
 
-        // Преобразуем в заглавные
+        // ГЏГ°ГҐГ®ГЎГ°Г Г§ГіГҐГ¬ Гў Г§Г ГЈГ«Г ГўГ­Г»ГҐ
         c = toupper(c);
         k = toupper(k);
 
@@ -70,66 +70,3 @@ string VizhenerDecode(string text, string key) {
 
 
 
-//#include "Vizhener.h"
-//#include <iostream>
-//#include <string>
-//
-//using namespace std;
-//
-//string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//
-//// функция получения кода символа
-//int keycode(char s) {
-//    for (int i = 0; i < alphabet.length(); i++) {
-//        if (s == alphabet[i]) return i;
-//    }
-//    return 0;
-//}
-//
-//string VizhenerEncode(string text, string key) {
-//    string code;
-//    for (int i = 0; i < text.length(); i++) {
-//        char c = text[i];
-//        char k = key[i % key.length()];
-//
-//        // Преобразуем в заглавные 
-//        c = toupper(c);
-//        k = toupper(k);
-//
-//        int c_code = keycode(c);
-//        int k_code = keycode(k);
-//
-//        if (c_code == 0 || k_code == 0) {
-//            // Обработка символов, отсутствующих в алфавите
-//            code += c; // Просто добавляем исходный символ
-//        }
-//        else {
-//            code += alphabet[(c_code + k_code) % alphabet.length()];
-//        }
-//    }
-//    return code;
-//}
-//
-//string VizhenerDecode(string text, string key) {
-//    string code;
-//    for (int i = 0; i < text.length(); i++) {
-//        char c = text[i];
-//        char k = key[i % key.length()];
-//
-//       
-//        c = toupper(c);
-//        k = toupper(k);
-//
-//        int c_code = keycode(c);
-//        int k_code = keycode(k);
-//
-//        if (c_code == 0 || k_code == 0) {
-//            
-//            code += c; 
-//        }
-//        else {
-//            code += alphabet[(c_code - k_code + alphabet.length()) % alphabet.length()];
-//        }
-//    }
-//    return code;
-//}
